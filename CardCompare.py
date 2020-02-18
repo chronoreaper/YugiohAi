61b0053f52801689b242cbdee08e234d4ca06d94
chronoreaper <chris.dd.29@gmail.com>
2020-02-18 15:19:20 -0500

    Update CardCompare.py

--- CardCompare.py
+++ CardCompare.py
@@ -1,15 +1,13 @@
 from difflib import SequenceMatcher
+import sqlite3, os
 
-def similar(a, b):
-    return SequenceMatcher(None, a, b).ratio()
+conn = sqlite3.connect(os.getcwd() +'/windbot_master/bin/Debug/cards.cdb')
 
-# import sqlite3, os
-
-# import textdistance
-
-# conn = sqlite3.connect(os.getcwd() +'/windbot_master/bin/Debug/cards.cdb')
-
-# c = conn.cursor()
+c = conn.cursor()
+text = ""
+for row in c.execute('SELECT * FROM texts where name="Monster Reborn"'):
+    print(row)
+    text = row[2]
 
 # card_select = c.execute('SELECT desc FROM texts where id=10000')
 # cardList =  {}# id to text
